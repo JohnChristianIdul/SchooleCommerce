801f5d08a8a2e4f2979f6cc1f5368d6c9d928431
earlBA <earl01000010@gmail.com>
2023-10-10 22:56:46 +0800

    test

--- SchooleCommerce/Merchant/models.py
+++ SchooleCommerce/Merchant/models.py
@@ -4,11 +4,13 @@ from Person.models import Person
 
 # Create your models here.
 class Merchant(Person):
+    merchant_ID = models.CharField(primary_key=True)
     pass
 
 
 class CustomerSupport(models.Model):
     pass
+
 
 
 class OrderConfirmation(models.Model):
